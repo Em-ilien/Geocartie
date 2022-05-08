71ff87679d192cf6a9b39aa1455208e72cf8ec7c
Em_i <emiliencoss@gmail.com>
2022-05-08 22:24:42 +0200

    quota google fix + project stucture

---
+++ admin/index.php
@@ -0,0 +1,42 @@
+<?php
+    if (!isset($_GET["code"]) || $_GET["code"] != "vivelageo")
+        die("Erreur : code invalide");
+
+    // Cette page web permet de récupérer les images du département sur Google Image via l'application console cloud google  API "Custom Search API"
+?>
+<script>
+	function getJSONFromFile(url) {
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', url, false);
+		xhr.send();
+		if (xhr.status != 200) {
+			console.log(xhr.status + ': ' + xhr.statusText);
+		} else {
+			return JSON.parse(xhr.responseText);
+		}
+	}
+
+    let json = getJSONFromFile("../data/departements.json");
+
+    for (var i = 0; i < json.length; i++) {
+        let departementInfos = json[i];
+
+        let req = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAD9PQpUwnjsJUAxwEPUuYeZk3v68Dww_I&cx=12d426587d6da4cbd&lr=lang_fr&searchType=image&q=" + encodeURIComponent(departementInfos.name + " département paysage")
+        let jsonGoogle = getJSONFromFile(req);
+    
+        for (var j = 0; j < jsonGoogle.items.length; j++) {
+            var item = jsonGoogle.items[j];
+    
+            let image = {
+                src: item.link,
+                description: item.snippet,
+                contextLink: item.image.contextLink
+            }
+            console.log(json[i])
+            json[i].images.push(image);
+        }
+    }
+
+    console.log(json);
+
+</script>
---
+++ main.js
@@ -0,0 +1,109 @@
+let departements = document.body.querySelectorAll(".departement");
+let infos = document.body.querySelector(".infos");
+
+let json = getJSONFromFile("data/departements.json");
+
+let petiteCouronne = document.querySelector(".paris-petite-couronne");
+let petiteCouronneagrandie = document.querySelector(".petite-couronne-agrandie");
+
+function getJSONFromFile(url) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.send();
+    if (xhr.status != 200) {
+        console.log(xhr.status + ': ' + xhr.statusText);
+    } else {
+        return JSON.parse(xhr.responseText);
+    }
+}
+
+departements.forEach(departement => {
+    departement.addEventListener("click", (e) => {
+        let activeDepartement = document.querySelector(".active");
+        if (activeDepartement)
+            activeDepartement.classList.remove("active");
+        if (petiteCouronne.classList.contains("active-paris"))
+            petiteCouronne.classList.remove("active-paris");
+        
+        departement.classList.add("active");
+        
+        let departementID = departement.classList[1];
+
+        let departementInfos = json.filter(function(data){ return data.id == departementID })[0];
+
+        infos.innerHTML = "";
+
+        let infosDepartementName = document.createElement("h2");
+        infosDepartementName.classList.add("departement-name");
+        infosDepartementName.innerText = departementInfos.name;
+        infos.appendChild(infosDepartementName);
+
+        let infosDepartementID = document.createElement("span");
+        infosDepartementID.innerText = departementInfos.id;
+        infosDepartementName.appendChild(infosDepartementID);
+
+        let infosDepartementRegion = document.createElement("p");
+        infosDepartementRegion.classList.add("region");
+        infosDepartementRegion.innerHTML = "Région : <span>" + departementInfos.region_name + "</span>";
+        infos.appendChild(infosDepartementRegion);
+
+        let infosDepartementPrefecture = document.createElement("p");
+        infosDepartementPrefecture.classList.add("prefecture");
+        infosDepartementPrefecture.innerHTML = "Préfecture : <span>" + departementInfos.prefecture_name + "</span>";
+        infos.appendChild(infosDepartementPrefecture);
+
+        let infosDepartementImages = document.createElement("div");
+        infosDepartementImages.classList.add("images");
+        infos.appendChild(infosDepartementImages);
+
+        departementInfos.images.forEach((image) => {
+            let imgEl = document.createElement("img");
+            imgEl.src = image.src;
+            imgEl.setAttribute("onclick", "window.open(`" + image.contextLink + "`, '_blank');");
+            imgEl.setAttribute("description", image.description);
+            infosDepartementImages.appendChild(imgEl);
+    
+            imgEl.addEventListener("mouseenter", (e) => {
+                let description = document.querySelector(".description");
+                description.innerHTML = e.target.getAttribute("description");
+                description.classList.add("show");
+                description.style.top = e.clientY + "px";
+                description.style.left = e.clientX + "px";
+            });
+    
+            imgEl.addEventListener("mouseout", (e) => {
+                let description = document.querySelector(".description");
+                description.classList.remove("show");
+            });
+        });
+
+        let voirPlusImages = document.createElement("a");
+        voirPlusImages.classList.add("voir-plus-images");
+        voirPlusImages.innerText = "Voir plus d'images sur Google";
+        voirPlusImages.href = "https://www.google.com/search?q=" + encodeURIComponent(departementInfos.name + " département paysage") + "&tbm=isch";
+        voirPlusImages.target = "_blank";
+        infos.appendChild(voirPlusImages);
+    });
+});
+
+petiteCouronne.addEventListener("click", (e) => {
+    for (let i = 0; i < 10; i++) {
+        if (i % 2 == 0) {
+            setTimeout(() => {
+                petiteCouronneagrandie.classList.add("active-paris");
+                petiteCouronne.classList.add("active-paris");
+            }, i*100);
+        } else {
+            setTimeout(() => {
+                petiteCouronneagrandie.classList.remove("active-paris");
+                petiteCouronne.classList.remove("active-paris");
+            }, i*100);
+        }	
+    }
+});
+
+for (const dep of petiteCouronneagrandie.children) {
+    dep.addEventListener("click", (e) => {
+        petiteCouronne.classList.add("active-paris");
+    });
+}
