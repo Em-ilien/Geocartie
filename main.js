68d5fbb9aafedbf83d20eabe4492652c138c65e6
Em_i <emiliencoss@gmail.com>
2022-06-11 14:20:58 +0200

    warning images added

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ let nameDepEl;
 let regionDepEl;
 let prefDepEl;
 let imagesDepCtn;
+let warningImages; //Avertissement temporaire pour prévenir de la non-fiabilité des images TODO
+let warningImagesSignalement; //TODO
 let buttonsLink;
 let seeMoreImagesBtn;
 let addImagesBtn;
@@ -110,6 +112,20 @@ function setupDepInfosEl() {
     imagesDepCtn.classList.add("images");
     infos.appendChild(imagesDepCtn);
 
+    warningImages = document.createElement("p"); //Temp TODO
+    warningImages.style.textAlign = "justify";
+    warningImages.style.fontSize = "0.7em";
+    warningImages.style.width = "75%";
+    warningImages.style.margin = "0 auto 2em";
+    warningImages.style.color = "#777";
+    warningImages.innerHTML = "Avertissement : ces images ont été extraites par un algorithme qui a parcouru le Web à leur recherche. Certaines d'entre elles sont susceptibles ne pas être pertinentes, dater, ne pas être libres de droit voire être associées à tort à ce département. Cette mosaïque a pour but de vous faire une idée grossière du département. ";
+    infos.appendChild(warningImages);
+
+    warningImagesSignalement = document.createElement("a"); //Temp TODO
+    warningImagesSignalement.innerText = "Signaler une image à supprimer.";
+    warningImagesSignalement.target = "_blank";
+    warningImages.appendChild(warningImagesSignalement);
+
     buttonsLink = document.createElement("div");
     buttonsLink.classList.add("buttons-link");
     infos.appendChild(buttonsLink);
@@ -161,8 +177,15 @@ function updateDepInfosEl(departementInfos) {
         });
     });
 
+    warningImagesSignalement.setAttribute("onclick", "javascript:window.open(`mailto:emilien.cosson.etu@univ-lemans.fr?subject=Suppression%20d%27une%20image%20sur%20G%C3%A9ocartie&body=Monsieur%20Cosson%2C%0D%0A%0D%0A%0D%0AJ'ai%20d%C3%A9couvert%20l'application%20Web%20nomm%C3%A9e%20G%C3%A9ocartie%20que%20vous%20avez%20r%C3%A9alis%C3%A9e.%0D%0A%0D%0AJe%20vous%20adresse%20ce%20courriel%20afin%20de" + encodeURIComponent(" vous signaler une ou plusieurs images à supprimer pour le département " + departementInfos.id + " (" + departementInfos.name + ") :\n\n- {LIEN_IMAGE}\n-\n-") + "%0D%0A%0D%0AJe%20devine%20l'attention%20que%20vous%20porterez%20%C3%A0%20mon%20message.%0D%0A%0D%0AMes%20sinc%C3%A8res%20salutations%2C%0D%0A%7BSIGNATURE%7D`, 'mail'); event.preventDefault();"); //Temp TODO
     seeMoreImagesBtn.href = "https://www.google.com/search?q=" + encodeURIComponent(departementInfos.name + " département paysage") + "&tbm=isch";
     addImagesBtn.setAttribute("onclick", "javascript:window.open(`mailto:emilien.cosson.etu@univ-lemans.fr?subject=Ajout%20d%27une%20image%20sur%20G%C3%A9ocartie&body=Monsieur%20Cosson%2C%0D%0A%0D%0A%0D%0AJ'ai%20d%C3%A9couvert%20l'application%20Web%20nomm%C3%A9e%20G%C3%A9ocartie%20que%20vous%20avez%20r%C3%A9alis%C3%A9e.%0D%0A%0D%0AJe%20vous%20adresse%20ce%20courriel%20afin%20de" + encodeURIComponent(" vous suggérer l'ajout d'images pour le département " + departementInfos.id + " (" + departementInfos.name + ") :\n\n- {LIEN_IMAGE}\n-\n-") + "%0D%0A%0D%0AJe%20devine%20l'attention%20que%20vous%20porterez%20%C3%A0%20mon%20message.%0D%0A%0D%0AMes%20sinc%C3%A8res%20salutations%2C%0D%0A%7BSIGNATURE%7D`, 'mail'); event.preventDefault();");
+    
+    //Temp TODO
+    if (departementInfos.verification)
+        warningImages.style.display = "none";
+    else
+        warningImages.style.display = "block";
 }
 
 function setupInfoBulle() {
