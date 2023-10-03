09709dc5b9568f24543cb76cac09a8ce2036db8f
Em_ilien <emilien@em-ilien.fr>
2023-10-03 10:01:52 +0200

    stick boottom navigation on smartphones

--- src/lib/components/sections/BottomNavigation.svelte
+++ src/lib/components/sections/BottomNavigation.svelte
@@ -14,10 +14,12 @@
     .bottom-navigation {
         display: flex;
         width: 100vw;
-        padding: 0 7.125em 0.375em 7.125em;
+        padding: 0 7.125em 0 7.125em;
         justify-content: center;
         align-items: flex-start;
         gap: 2.25em;
+        position: fixed;
+        bottom: 0;
 
         background: #FFF;
         box-shadow: 0 -1px 4px 0 rgba(0, 0, 0, 0.25);
