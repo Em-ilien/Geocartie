0516eca94cfbb16735af9501ea23cebc855e0ac7
Emilien Cosson <emilien@em-ilien.fr>
2023-11-05 18:43:15 +0100

    add duration to toast message

--- src/lib/helpers/toasts.js
+++ src/lib/helpers/toasts.js
@@ -2,12 +2,12 @@ import toast from 'svelte-french-toast';
 
 const STYLE = "font-family: 'Roboto', 'Lato', sans-serif; box-shadow: 2px 2px 5px 3px #00000025";
 const POSITION = 'top-right';
-const QUIZZ_POSITION = 'bottom-right';
 
 export function onClickLogin(e) {
 	toast.error("La connexion n'est pas encore implémentée.", {
 		style: STYLE,
 		position: POSITION,
+		duration: 2000,
 	});
 }
 
@@ -15,5 +15,6 @@ export function onClickMenuNav(e, itemLabel) {
 	toast.error('Le menu ' + itemLabel + " n'est pas encore implémenté.", {
 		style: STYLE,
 		position: POSITION,
+		duration: 2000,
 	});
 }
